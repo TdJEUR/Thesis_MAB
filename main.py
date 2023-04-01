0c3ee1359603494dc0e285acfcea5fb5ae733646
tomjdejonge <tomjacobdejonge@gmail.com>
2023-04-01 16:53:46 +0200

    Small edits

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from SoftMaxMAB import SoftmaxMAB
 import matplotlib.pyplot as plt
 
-true_arm_rewards = [0.4, 0.8, 0.4]
-true_arm_stds = []
-beliefs = [0, 0, 0]
-number_of_rounds = 10
+true_arm_rewards = [0.4, 0.8, 0.4, 0.3]
+# true_arm_stds = []
+number_of_rounds = 100
 tau = 10
 alpha = 0.5
 
 if __name__ == '__main__':
+
     bandit = SoftmaxMAB(true_arm_rewards, alpha, tau)
     final_beliefs, total_reward, accumulated_reward = bandit.play(number_of_rounds)
     plt.plot(range(number_of_rounds), accumulated_reward)
