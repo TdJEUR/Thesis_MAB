c9a181e19d7e1623bd887c370366cec92fbbcea4
tomjdejonge <tomjacobdejonge@gmail.com>
2023-04-05 21:23:11 +0200

    Add plots to team_mab

--- Run_Team_MAB.py
+++ Run_Team_MAB.py
@@ -18,19 +18,15 @@ alphas = [0.5, 0.5, 0.5, 0.5]
 # tau = 0.9
 
 if __name__ == '__main__':
+    # Run simulation with constant alpha and different values of tau
     acc_rewards = []
     for i in np.linspace(start=0.01, stop=3, num=5):
         acc_rewards.append(team_MAB(alphas, i, true_arm_rewards, number_of_rounds))
-
-    # set the x and y axis labels
+    # Plot results
     plt.xlabel('Round number')
     plt.ylabel('Reward gained')
-    # set the title of the plot
     plt.title('Reward gained over time')
-    # plot each y dataset
     for i in range(len(acc_rewards)):
         plt.plot(range(number_of_rounds), acc_rewards[i], label=f'Tau {i + 1}')
-    # show the legend
     plt.legend()
-    # display the plot
     plt.show()
